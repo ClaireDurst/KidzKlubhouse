57441bc97763761abb6cf3420eb2b1660a1217cf
Claire Durst <clairedurst@gmail.com>
2017-08-15 21:24:15 -0400

    imgur

--- assets/javascript/kidzklubhouse.js
+++ assets/javascript/kidzklubhouse.js
@@ -1,9 +1,46 @@
+  $(function () {
+      $("form").on("submit", function(e) {
+          displayImages();
+      });
+  });
+var submit = $("idtsearch").val();
+function displayImages(){
+var settings = {
+  async: true,
+  crossDomain: true,
+  url: "https://api.imgur.com/3/gallery/search/?q=" + submit + "", 
+  method: "GET",
+  headers: {
+    authorization: "Client-ID f68be402270b032"
+  }
+}
 
-$(function () {
+$.ajax(settings).done(function (response) {
+  console.log(response);
+ $("#imagessView").empty();
+for (var i = 0; i < 5; i++) {
+        var results = response.data[i];
+        console.log(results);
+console.log(results.link);
 
- $(document).ready(function(){
-      $('.carousel').carousel();
-    });
-   $('.slider').slider();
-});
+var imageDiv = $("<div />");
+imageDiv.addClass("imageDiv");
+var title = results.title;
+console.log(title)
+console.log(results.images[i].link)
+var personImage = $("<img>");
+personImage.attr("src",results.images[i].link)
+personImage.addClass("img");
+var imagecap = $("<div />").text("");
+imageDiv.append(personImage);
+$("#imageResults").append(personImage);
 
+
+}
+
+        });
+    };
+
+
+
+displayImages();
